11954ef7be415bd06796e4c8b6940ce6ea062cf6
Chris Cankov <cankov.chris@gmail.com>
2017-06-27 18:57:24 -0700

    Add random user seeds

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+      DB::table('users')->insert([
+          'firstname' => str_random(10),
+          'lastname' => str_random(10),
+          'favorite_color' => str_random(10),
+          'email' => str_random(10).'@gmail.com',
+          'password' => bcrypt('secret'),
+      ]);
+
+      DB::table('users')->insert([
+          'firstname' => str_random(10),
+          'lastname' => str_random(10),
+          'favorite_color' => str_random(10),
+          'email' => str_random(10).'@gmail.com',
+          'password' => bcrypt('secret'),
+      ]);
+
+      DB::table('users')->insert([
+          'firstname' => str_random(10),
+          'lastname' => str_random(10),
+          'favorite_color' => str_random(10),
+          'email' => str_random(10).'@gmail.com',
+          'password' => bcrypt('secret'),
+      ]);
+    }
+}
