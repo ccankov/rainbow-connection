9714aa4cf0e4d2912b712623a60b813095773908
Chris Cankov <cankov.chris@gmail.com>
2017-06-28 15:08:49 -0700

    Attempt to configure Ember adapters

--- resources/frontend/app/adapters/user.js
+++ resources/frontend/app/adapters/user.js
@@ -4,5 +4,11 @@ import Ember from 'ember';
 export default DS.Adapter.extend({
   query(modelName, query) {
     return Ember.$.getJSON(`/api/users?page=1`);
+  },
+  queryRecord(modelName, query) {
+    console.log(query);
+    console.log(modelName);
+    debugger;
+    return Ember.$.getJSON(`/api/users/${query.id}`);
   }
 });
--- resources/frontend/app/routes/user.js
+++ resources/frontend/app/routes/user.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(params) {
-    return this.get('store').queryRecord('user', params.user_id);
+    debugger;
+    return this.get('store').queryRecord('user', params);
   }
 });
