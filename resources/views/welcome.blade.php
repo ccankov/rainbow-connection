d0d1f88235d5569344999f271dc56395deb51218
Chris Cankov <cankov.chris@gmail.com>
2017-06-27 22:40:13 -0700

    Initialize an ember project and integrate with laravel view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,8 +2,11 @@
 <html>
     <head>
         <title>Rainbow Connection</title>
+        <meta name="frontend/config/environment" content="%7B%22modulePrefix%22%3A%22frontend%22%2C%22environment%22%3A%22development%22%2C%22rootURL%22%3A%22/%22%2C%22locationType%22%3A%22auto%22%2C%22EmberENV%22%3A%7B%22FEATURES%22%3A%7B%7D%2C%22EXTEND_PROTOTYPES%22%3A%7B%22Date%22%3Afalse%7D%7D%2C%22APP%22%3A%7B%22name%22%3A%22frontend%22%2C%22version%22%3A%220.0.0+253be3eb%22%7D%2C%22exportApplicationGlobal%22%3Atrue%7D" />
 
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" href="/dist/assets/vendor.css">
+        <link rel="stylesheet" href="/dist/assets/frontend.css">
 
         <style>
             html, body {
@@ -36,10 +39,7 @@
         </style>
     </head>
     <body>
-        <div class="container">
-            <div class="content">
-                <div class="title">Rainbow Connection</div>
-            </div>
-        </div>
+        <script type="text/javascript" src="/dist/assets/vendor.js"></script>
+        <script type="text/javascript" src="/dist/assets/frontend.js"></script>
     </body>
 </html>
